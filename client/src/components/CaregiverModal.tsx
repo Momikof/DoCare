0c48582f381ac9f64ba9d0d4de89889364b029e4
guom2003@gmail.com <guom2003@gmail.com>
2023-09-10 22:15:29 +0300

    final caregiver modal

--- client/src/components/CaregiverModal.tsx
+++ client/src/components/CaregiverModal.tsx
@@ -1,6 +1,10 @@
 import { View } from "react-native";
+import { Text } from "@design-system";
 import { useScreenSize } from "../hooks/useScreenSize";
 import { ReactNativeModal } from "react-native-modal";
+import CreateGroupSVG from "@images/create-group.svg";
+import JoinGroupSVG from "@images/join-group.svg";
+import BabyCoupleSVG from "@images/baby-couple.svg";
 
 type CaregiverModalProps = {
   isVisible: boolean;
@@ -30,7 +34,56 @@ export function CaregiverModal({
           borderWidth: 1,
           borderColor: "#1F1F1F",
         }}
-      ></View>
+      >
+        <View style={{ flexDirection: "row" }}>
+          <JoinGroupSVG
+            style={{
+              maxHeight: height * 0.15,
+              maxWidth: width * 0.4,
+              marginLeft: width * 0.05,
+              marginTop: height * 0.05,
+            }}
+          />
+
+          <View
+            style={{
+              position: "absolute",
+              marginTop: height * 0.1,
+              marginLeft: width * 0.08,
+            }}
+          >
+            <Text variant="secondary">הצתרף לקבוצת</Text>
+            <Text variant="secondary">טיפול קיימת</Text>
+          </View>
+
+          <CreateGroupSVG
+            style={{
+              maxHeight: height * 0.15,
+              maxWidth: width * 0.4,
+              marginLeft: width * 0.05,
+              marginTop: height * 0.05,
+            }}
+          />
+          <View
+            style={{
+              position: "absolute",
+              marginTop: height * 0.1,
+              marginLeft: width * 0.58,
+            }}
+          >
+            <Text variant="secondary">פתח קבוצת</Text>
+            <Text variant="secondary">טיפול חדשה</Text>
+          </View>
+        </View>
+        <BabyCoupleSVG
+          style={{
+            maxHeight: height * 0.4,
+            maxWidth: width * 0.9,
+            marginLeft: width * 0.02,
+            marginTop: height * 0.03,
+          }}
+        />
+      </View>
     </ReactNativeModal>
   );
 }
