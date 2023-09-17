097c0f197d52af8111307af53eff81d2e4c81b24
ofek-droid <ofekitach2@gmail.com>
2023-09-17 21:31:52 +0300

    initizlied onesignal in client

--- client/src/components/one-signal-test/one-signal-test.tsx
+++ client/src/components/one-signal-test/one-signal-test.tsx
@@ -1,129 +1,22 @@
-import { createClient, User } from '@supabase/supabase-js'
 import type { NextPage } from 'next'
-import Head from 'next/head'
 import React, { useEffect, useState } from 'react'
-import OneSignal from 'react-onesignal'
+import { View } from '../../design-system'
+import {LogLevel, OneSignal} from 'react-native-onesignal'
+import Constants from 'expo-constants'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-const oneSignalAppId = process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID!
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey)
+// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+// const oneSignalAppId = process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID!
 
 export const Home: NextPage = () => {
-  const [user, setUser] = useState<User | null>(null)
-
-  const [oneSignalInitialized, setOneSignalInitialized] = useState<boolean>(false)
-
-  /**
-   * Initializes OneSignal SDK for a given Supabase User ID
-   * @param uid Supabase User ID
-   */
-  const initializeOneSignal = async (uid: string) => {
-    if (oneSignalInitialized) {
-      return
-    }
-    setOneSignalInitialized(true)
-    await OneSignal.init({
-      appId: oneSignalAppId,
-      notifyButton: {
-        enable: true,
-      },
-
-      allowLocalhostAsSecureOrigin: true,
-    })
-
-    await OneSignal.login(uid)
-  }
-
-  const sendMagicLink = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    console.log({...event.currentTarget});
-   // const { email } = Object.fromEntries(new FormData(event.currentTarget) as any)
-   // if (typeof email !== 'string') return
-
-    //const { error } = await supabase.auth.signInWithOtp({ email })
-    // (error) {
-    //  alert(error.message)
-   // } else {
-    //  alert('Check your email inbox')
-   // }
-  }
-
-  // Place a order with the selected price
-  const submitOrder = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    //const { price } = Object.fromEntries(new FormData(event.currentTarget) as any)
-    //if (typeof price !== 'string') return
-
-    //const { error } = await supabase.from('orders').insert({ price: Number(price) })
-    //if (error) {
-    //  alert(error.message)
-   // }
-  }
-
   useEffect(() => {
-    const initialize = async () => {
-      const initialUser = (await supabase.auth.getUser())?.data.user
-      setUser(initialUser ?? null)
-      if (initialUser) {
-        initializeOneSignal(initialUser.id)
-      }
-    }
+    OneSignal.Debug.setLogLevel(LogLevel.Debug);
+    OneSignal.initialize(Constants.expoConfig.extra.oneSignalAppId);
+    OneSignal.Notifications.requestPermission(true);
+  }, []);
+  return <View>
 
-    initialize()
-
-    const authListener = supabase.auth.onAuthStateChange(async (event, session) => {
-      const user = session?.user ?? null
-      setUser(user)
-      if (user) {
-        initializeOneSignal(user.id)
-      }
-    })
-
-    return () => {
-      authListener.data.subscription.unsubscribe()
-    }
-  }, [])
-
-  return (
-    <>
-      <Head>
-        <title>OneSignal Order Notification App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex items-center justify-center min-h-screen bg-black">
-        {user ? (
-          <form className="flex flex-col space-y-2" onSubmit={submitOrder}>
-            <select
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded block p-2"
-              name="price"
-            >
-              <option value="100">$100</option>
-              <option value="200">$200</option>
-              <option value="300">$300</option>
-            </select>
-            <button type="submit" className="py-1 px-4 text-lg bg-green-400 rounded">
-              Place an Order
-            </button>
-          </form>
-        ) : (
-          <form className="flex flex-col space-y-2" onSubmit={sendMagicLink}>
-            <input
-              className="border-green-300 border rounded p-2 bg-transparent text-white"
-              type="email"
-              name="email"
-              placeholder="Email"
-            />
-            <button type="submit" className="py-1 px-4 text-lg bg-green-400 rounded">
-              Send Magic Link
-            </button>
-          </form>
-        )}
-      </main>
-    </>
-  )
+  </View>
 }
 
 export default Home
