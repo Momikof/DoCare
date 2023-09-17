00f9ef8af900a84db73adee4a34f1d652f60d30f
ofek-droid <ofekitach2@gmail.com>
2023-09-18 00:21:59 +0300

    implemented edge function for sending notifications

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # DoCare
 
+## Requirements
+if you want to work on the edge functions make sure you have:
+- Supabase CLI: https://github.com/supabase/cli <br>
+- Deno: https://deno.com/ <br>
+
 ## Starting the client
 
 Start the client<br>
@@ -11,3 +16,7 @@ Next you will need to create a .env file:<br>
 `SUPABASE_URL=<SOME-URL-VALUE>`<br>
 `SUPABASE_PUBLIC_ANNO_KEY=<SOME-ANNO-VALUE>`<br>
 
+Create a root .env if you develop the edge functions locally:<br>
+`ONESIGNAL_APP_ID=YOUR_ONESIGNAL_APP_ID`<br>
+`USER_AUTH_KEY=YOUR_USER_AUTH_KEY`<br>
+`ONESIGNAL_REST_API_KEY=YOUR_ONESIGNAL_REST_API_KEY`<br>
--- supabase/functions/send-notification/index.ts
+++ supabase/functions/send-notification/index.ts
@@ -1,21 +1,36 @@
-import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
+import * as OneSignal from 'https://esm.sh/@onesignal/node-onesignal@1.0.0-beta7';
+import { Database } from "../../types/supabase.ts";
 
-console.log("Hello from Functions!")
-
-serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!`,
-  }
-
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
+const _OnesignalAppId_ = Deno.env.get('ONESIGNAL_APP_ID')!
+const _OnesignalUserAuthKey_ = Deno.env.get('USER_AUTH_KEY')!
+const _OnesignalRestApiKey_ = Deno.env.get('ONESIGNAL_REST_API_KEY')!
+const configuration = OneSignal.createConfiguration({
+  userKey: _OnesignalUserAuthKey_,
+  appKey: _OnesignalRestApiKey_,
 })
 
-// To invoke:
-// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
-//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-//   --header 'Content-Type: application/json' \
-//   --data '{"name":"Functions"}'
+const onesignal = new OneSignal.DefaultApi(configuration);
+
+serve(async (req) => {
+  try {
+    const record: Database['public']['Tables']['tasks']['Row'] = (await req.json()).record;
+    const notification = new OneSignal.Notification()
+    notification.app_id = _OnesignalAppId_
+    notification.include_external_user_ids = [record.accepted_by as string];
+    notification.contents = {
+      en: `New task has been created: ${record.title}!`,
+    }
+    const onesignalApiRes = await onesignal.createNotification(notification);
+
+    return new Response(JSON.stringify({ onesignalResponse: onesignalApiRes }), {
+      headers: { 'Content-Type': 'application/json' },
+    })
+  } catch (err) {
+    console.error('Failed to create OneSignal notification', err)
+    return new Response('Server error.', {
+      headers: { 'Content-Type': 'application/json' },
+      status: 400,
+    })
+  }
+})
