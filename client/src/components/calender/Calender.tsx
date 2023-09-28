e4d5b6f933b53df2acb0a63a40fa7f0433acc6b2
dvir <dvirhamdi@gmail.com>
2023-09-28 21:58:23 +0300

    export to const

--- client/src/components/calender/Calender.tsx
+++ client/src/components/calender/Calender.tsx
@@ -5,12 +5,13 @@ import { StyleSheet, View} from 'react-native';
 import {Text} from '@design-system'
 import CalendarPicker from 'react-native-calendar-picker';
 
-const CALENDER_TITLE = 'לו ח שנה'
+const CALENDER_TITLE = 'לו ח שנה';
+const DATE_FORNAT = 'DD-MM-YYYY';
 
 export const Calender = () => {
     const [selectedStartDate, setSelectedStartDate] = useState<any>();
     const startDate = selectedStartDate
-      ? selectedStartDate.format('DD-MM-YYYY').toString()
+      ? selectedStartDate.format(DATE_FORNAT).toString()
       : '';
   
     return (
